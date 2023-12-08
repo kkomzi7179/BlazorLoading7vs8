b1c75a04ea70e0cf070a02580bd56f7c7b003f23
Jaeyong Park <kkomzi@gmail.com>
2023-12-08 19:45:19 +0900

    - Add missing files

---
+++ DotNet8BlazorWebApp/DotNet8BlazorWebApp.Client/IJSRuntimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace DotNet8BlazorWebApp.Client
+{
+    public static partial class IJSRuntimeExtensions
+    {
+        public static async Task DeleteElementById(this IJSRuntime jsRuntime, string id)
+        {
+            await jsRuntime.InvokeVoidAsync("DeleteElementById", id);
+        }
+    }
+}
